e7079b0aaf936efebffe70cfc892425e204c745e
Justin <jpowers4877@floridapoly.edu>
2018-12-04 14:40:54 -0500

    Implement Initialize Method; Suppress Duplicates

--- src/main/controller/ControllerNF.java
+++ src/main/controller/ControllerNF.java
@@ -3,16 +3,25 @@ package main.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerNF
+public class ControllerNF implements Initializable
 {
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
 
+    }
+
+    @SuppressWarnings("Duplicates")
     @FXML
     void mainMenu(ActionEvent event) throws IOException
     {
